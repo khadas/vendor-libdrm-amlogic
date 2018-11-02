0733c43d051cac69e17c7298543be1ee25a0a72f
Ao Xu <ao.xu@amlogic.com>
2018-11-02 17:35:02 +0800

    drm: add platform related head file [1/1]
    PD#SWPL-87
    
    Problem:
    some application depend on the head files.
    
    Solution:
    add the related head files.
    
    Verify:
    no need to test.
    
    Change-Id: I841e63176584179f8897b5d9b42724237fe79640
    Signed-off-by: Ao Xu <ao.xu@amlogic.com>

--- meson_drm.h
+++ meson_drm.h
@@ -1,51 +1,50 @@
 /*
- * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
+ * include/uapi/drm/meson_drm.h
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+ * Copyright (C) 2016 Amlogic, Inc. All rights reserved.
  *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
  */
 
-#ifndef _MESON_DRM_H_
-#define _MESON_DRM_H_
+#ifndef _MESON_DRM_H
+#define _MESON_DRM_H
 
-#include <stdint.h>
-#include "drm.h"
+#include <drm/drm.h>
+
+/* Use flags */
+#define MESON_USE_NONE			0
+#define MESON_USE_SCANOUT			(1ull << 0)
+#define MESON_USE_CURSOR			(1ull << 1)
+#define MESON_USE_RENDERING		(1ull << 2)
+#define MESON_USE_LINEAR			(1ull << 3)
+#define MESON_USE_PROTECTED		(1ull << 11)
+#define MESON_USE_HW_VIDEO_ENCODER		(1ull << 12)
+#define MESON_USE_CAMERA_WRITE		(1ull << 13)
+#define MESON_USE_CAMERA_READ		(1ull << 14)
+#define MESON_USE_TEXTURE			(1ull << 17)
+
 
 /**
  * User-desired buffer creation information structure.
  *
  * @size: user-desired memory allocation size.
- *	- this size value would be page-aligned internally.
  * @flags: user request for setting memory type or cache attributes.
  * @handle: returned a handle to created gem object.
- *	- this handle will be set by gem module of kernel side.
+ *     - this handle will be set by gem module of kernel side.
  */
 struct drm_meson_gem_create {
-	uint64_t size;
-	uint32_t flags;
-	uint32_t handle;
+	__u64 size;
+	__u32 flags;
+	__u32 handle;
 };
 
-#define DRM_MESON_GEM_CREATE	0x00
+#define DRM_MESON_GEM_CREATE		0x00
 
-#define DRM_IOCTL_MESON_GEM_CREATE DRM_IOWR(DRM_COMMAND_BASE + \
+#define DRM_IOCTL_MESON_GEM_CREATE		DRM_IOWR(DRM_COMMAND_BASE + \
 		DRM_MESON_GEM_CREATE, struct drm_meson_gem_create)
 
-#endif
+#endif /* _MESON_DRM_H */
+
